62ff842ab48f14321f4e1090d944b56c7365c3f7
bugczw <8747734+bugczw@user.noreply.gitee.com>
2021-12-18 12:40:49 +0000

    update maxp_model_czw/dataset/demo4.py.

--- maxp_model_czw/dataset/demo4.py
+++ maxp_model_czw/dataset/demo4.py
@@ -60,7 +60,7 @@ def feat_map(i):
 localtime = time.asctime(time.localtime(time.time()))
 print('Start Extract', localtime)
 
-workers = 50
+workers = 20
 with Pool(workers) as p:
     rets = p.map(feat_map, range(graph.num_nodes()))
     
