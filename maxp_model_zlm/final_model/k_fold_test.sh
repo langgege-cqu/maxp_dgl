101e6c4c86ac3da409e08994843a2838c72b5c2e
Laoming Zhang <lpancake@foxmail.com>
2021-12-23 06:03:34 +0000

    final

--- maxp_model_zlm/final_model/k_fold_test.sh
+++ maxp_model_zlm/final_model/k_fold_test.sh
@@ -1,2 +1,2 @@
 cd unimp
-CUDA_VISIBLE_DEVICES=3 python test_yaml_k_fold.py --cfg_file ../dgl_models/final/deepwalk1_gat1_se/config.yaml
+CUDA_VISIBLE_DEVICES=0 python test_yaml_k_fold.py --cfg_file ../dgl_models/final/order_deepwalk2_gat1_se/config.yaml
