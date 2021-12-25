0c7ba547c7dfd9cc9abfbb4e3809519b3761aaf5
bugczw <8747734+bugczw@user.noreply.gitee.com>
2021-12-25 07:28:55 +0000

    删除文件 maxp_model

--- maxp_model/deepwalk/run.sh
+++
@@ -1,1 +0,0 @@
- python deepwalk.py --data_file ../../data/graph.bin --output_emb_file ../../data/deepwalk.npy --mix --lr 0.2 --gpus 0 1 2 3 --batch_size 2000 --negative 6
--- maxp_model/unimp/demo/run.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train_yaml.py --cfg_file demo/config.yaml
-
-CUDA_VISIBLE_DEVICES=0 python test_yaml.py --cfg_file demo/config.yaml
