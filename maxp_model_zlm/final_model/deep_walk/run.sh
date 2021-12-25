90f89d90162c88e2aa64fb939d4727f562b4630f
bugczw <8747734+bugczw@user.noreply.gitee.com>
2021-12-25 07:29:16 +0000

    删除文件 maxp_model_zlm

--- maxp_model_zlm/final_model/deep_walk/run.sh
+++
@@ -1,3 +0,0 @@
-python deepwalk.py --data_file ../final_dataset/graph.bin --output_emb_file ../final_dataset/deep_walk1.npy --mix --gpus 0 1 2 3 4 5 6 7 --batch_size 5000 --window_size 5 --num_walks 50 --negative 1 --neg_weight 1 --walk_length 80  --lap_norm 0.01 --lr 0.1 --use_context_weight
-
-python deepwalk.py --data_file ../final_dataset/graph.bin --output_emb_file ../final_dataset/deep_walk2.npy --mix --gpus 0 1 2 3 4 5 6 7 --batch_size 5000 --window_size 5 --num_walks 50 --negative 1 --neg_weight 1 --walk_length 100 --lap_norm 0.01 --lr 0.1 --use_context_weight
--- maxp_model_zlm/final_model/k_fold_test.sh
+++
@@ -1,2 +0,0 @@
-cd unimp
-CUDA_VISIBLE_DEVICES=0 python test_yaml_k_fold.py --cfg_file ../dgl_models/final/order_deepwalk2_gat1_se/config.yaml
